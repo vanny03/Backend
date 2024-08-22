cce93d8727d956e517bae16b16ad021dd75f1d10
vanny03 <chanvanny044@gmail.com>
2024-08-22 23:52:33 +0700

    update code

--- src/routes/postRoutes.ts
+++ src/routes/postRoutes.ts
@@ -1,1 +1,38 @@
 import { UserController } from "../controller/UserController.js"
+
+export const productRoutes = [
+    {
+        method: "get",
+        route: "/products",
+        controller: UserController,
+        action: "getAllProduct"
+    }, 
+    
+    {
+        method: "get",
+        route: "/products/:id",
+        controller: UserController,
+        action: "oneProduct"
+    },
+    
+    {
+        method: "post",
+        route: "/products",
+        controller: UserController,
+        action: "saveProduct"
+    },
+
+    // Update user
+    {
+        method: "put",
+        route: "/products",
+        controller: UserController,
+        action: "updateProduct"
+    },
+   
+    {
+        method: "delete",
+        route: "/products/:id",
+        controller: UserController,
+        action: "removeProduct"
+    }]
